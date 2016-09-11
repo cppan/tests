35860aaa0c4c293366d178af2dce69ae1e69b378
Egor Pugin <egor.pugin@gmail.com>
2016-09-11 22:34:37 +0300

    More tests.

---
+++ tests/ok/tinyxml.cpp
@@ -0,0 +1,10 @@
+/*
+dependencies:
+    - pvt.cppan.demo.tinyxml
+*/
+
+#include <tinyxml.h>
+
+int main(int argc, char* argv[])
+{
+}
---
+++ tests/ok/tinyxml2.cpp
@@ -0,0 +1,10 @@
+/*
+dependencies:
+    - pvt.cppan.demo.tinyxml2
+*/
+
+#include <tinyxml2.h>
+
+int main(int argc, char* argv[])
+{
+}
