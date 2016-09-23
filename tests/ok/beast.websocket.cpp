232ed86faefcb0d509c77245d3f0a1ba03d5c1a5
Egor Pugin <egor.pugin@gmail.com>
2016-09-24 01:38:29 +0300

    More tests.

---
+++ tests/ok/beast.websocket.cpp
@@ -0,0 +1,37 @@
+/*
+local_settings:
+    build:
+        use_shared_libs: true
+dependencies:
+    pvt.cppan.demo.vinniefalco.beast: master
+    pvt.cppan.demo.boost.system: 1
+*/
+
+#include <beast/core/to_string.hpp>
+#include <beast/websocket.hpp>
+#include <boost/asio.hpp>
+#include <iostream>
+#include <string>
+
+int main()
+{
+    // Normal boost::asio setup
+    std::string const host = "echo.websocket.org";
+    boost::asio::io_service ios;
+    boost::asio::ip::tcp::resolver r{ios};
+    boost::asio::ip::tcp::socket sock{ios};
+    boost::asio::connect(sock,
+        r.resolve(boost::asio::ip::tcp::resolver::query{host, "80"}));
+
+    // WebSocket connect and send message using beast
+    beast::websocket::stream<boost::asio::ip::tcp::socket&> ws{sock};
+    ws.handshake(host, "/");
+    ws.write(boost::asio::buffer("Hello, world!"));
+
+    // Receive WebSocket message, print and close using beast
+    beast::streambuf sb;
+    beast::websocket::opcode op;
+    ws.read(op, sb);
+    ws.close(beast::websocket::close_code::normal);
+    std::cout << to_string(sb.data()) << "\n";
+}
