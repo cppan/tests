579f64503062f194eb56cbb5acb27bf0b2b93ffd
Egor Pugin <egor.pugin@gmail.com>
2016-10-03 23:05:54 +0300

    Add aws tests.

---
+++ tests/ok/aws.sdk.core.cpp
@@ -0,0 +1,14 @@
+/*
+local_settings:
+    build:
+        use_shared_libs: true
+dependencies:
+    pvt.cppan.demo.amazon.aws.sdk.core
+*/
+
+#include <aws/core/Aws.h>
+
+int main()
+{
+    return 0;
+}
